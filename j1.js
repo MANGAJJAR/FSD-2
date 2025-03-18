0e5127b7ceec8285779eed0a2ca69c150c79d7aa
Man Gajjar <140699930+MANGAJJAR@users.noreply.github.com>
2025-03-18 10:42:20 +0530

    Add files via upload

---
+++ j1.js
@@ -0,0 +1,16 @@
+// var obj = {'Marks': 97, 'name': 'man', 'year': '44'}
+// console.log(obj)
+
+
+var a = {
+        'name' : 'Test',
+        'age' : 26,
+        'ispass': false,
+        'Address' : {'City': 'Ahm','Zip': 380026},
+        'Subject' : ["Maths",'FSD-2'],
+        'Year': null
+    }
+    console.log(a.age)
+
+    console.log(a.Address.City)
+    console.log(a['Address']['City'])
---
+++ use-data-otherfile.js
@@ -0,0 +1,2 @@
+const data = require("./1.json")
+console.log(data.name)
